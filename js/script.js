8415bb3e8d457131503d85e98e0c1c6193daa807
Rayan <renomohamed16@gmail.com>
2025-09-10 21:18:57 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".swiper", {
+    spaceBetween: 10,
+    loop: true,
+    speed: 10000,
+    slidesPerView: "auto",
+    freeMode: true,
+    autoplay: {
+        delay: 0,
+    },
+});
+
+
+
+
